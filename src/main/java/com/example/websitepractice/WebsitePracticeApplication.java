e3e202b84cb519fe503afa8369b167a29eafd4e8
Katzuwu <ian.g.arellano01@gmail.com>
2022-02-21 19:19:08 -0600

    initial commit. want to make sure git ignore works

---
+++ src/main/java/com/example/websitepractice/WebsitePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.websitepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsitePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websitepractice/WebsitePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websitepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
